38b5580b4ae500cc9173192cd28c9f27c24c24a6
Valentin0803 <21701926@etu.unicaen.fr>
2020-02-04 14:24:56 +0100

    Entité Organization

---
+++ TD2/td2/src/main/java/s4/spring/td2/entities/Organization.java
@@ -0,0 +1,17 @@
+package s4.spring.td2.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Organization {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+
+    private String name;
+    private String domain;
+    private String aliases;
+}
