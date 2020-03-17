363a21d864545ce876707e70e8b3328de1ddee18
21701926 <21701926@etu.unicaen.fr>
2020-03-18 00:08:40 +0100

    creation td5

---
+++ td5/src/main/java/s4/spring/td5/ServletInitializer.java
@@ -0,0 +1,13 @@
+package s4.spring.td5;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Td5Application.class);
+	}
+
+}
---
+++ td5/src/test/java/s4/spring/td5/Td5ApplicationTests.java
@@ -0,0 +1,13 @@
+package s4.spring.td5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
